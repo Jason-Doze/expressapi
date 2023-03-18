bf9a92f930af19ebd2b5777a76d173d94085b869
Jason Doze <dozejason@gmail.com>
2023-03-18 12:53:27 -0500

    Add html and photo to repo.
    Add reload joke button.
    
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 // Enable CORS requests
 app.use(cors());
 
+// Set up a middleware to serve static files from the public folder
+app.use(express.static('public'));
+
 // Define a new HTTP GET endpoint at /joke
 app.get('/joke', async (req, res) => {
   try {
@@ -15,9 +18,18 @@ app.get('/joke', async (req, res) => {
     const data = await response.json();
     const joke = data.value;
     // Display the joke in the console
+    const image = '/images/chuck-norris.jpg';
+    // Optionally, send the joke and image URL as an HTTP response to the client
+    const html = `
+      <div id="joke"; style="text-align: center; margin-top: 50px;">
+      <h1 style="text-align: center; margin-top: 25px;">${joke}</h1>
+          <img src="${image}" alt="Chuck Norris" style="display: block; margin: 100px auto 0;"/>
+          <button onclick="reloadJoke()" style="margin-top: 10px;">Get Another Joke</button>
+      </div>
+      <script src="/script.js"></script>
+    `;
+    res.send(html);
     console.log(joke);
-    // Optionally, send the joke as an HTTP response to the client
-    res.send(joke);
   } catch (error) {
     // Handle any errors or exceptions that may occur
     console.error(error);
@@ -29,4 +41,3 @@ app.get('/joke', async (req, res) => {
 app.listen(3000, () => {
   console.log('API running on port 3000');
 });
-
