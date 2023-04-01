9884458b858213679caf5d053b6052ca4c890db0
Jason Doze <dozejason@gmail.com>
2023-04-01 15:13:22 -0500

    Add html file.
    Add photo as favicon.ico.
    
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 const app = express();
+
 // Enable CORS requests
 app.use(cors());
 
 // Set up a middleware to serve static files from the public folder
 app.use(express.static('public'));
 
+// Serve the favicon
+app.get('/favicon.ico', (req, res) => {
+  const faviconPath = './public/images/favicon.ico';
+
+  // Read the contents of the favicon file using fs.readFile
+  fs.readFile(faviconPath, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(404);
+    } else {
+      // Set the content type to image/x-icon
+      res.set('Content-Type', 'image/x-icon');
+
+      // Send the favicon file as a response
+      res.send(data);
+    }
+  });
+});
+
 // Define a new HTTP GET endpoint at /joke
 app.get('/joke', async (req, res) => {
   try {
-    // Send an HTTP GET request to the Chuck Norris API and await the response
     const response = await fetch('https://api.chucknorris.io/jokes/random');
-    // Parse the response as JSON and extract the joke
     const data = await response.json();
     const joke = data.value;
-    // Display the joke in the console
     const image = '/images/chuck-norris.jpg';
-    // Optionally, send the joke and image URL as an HTTP response to the client
-    const html = `
-      <div id="joke"; style="text-align: center; margin-top: 50px;">
-      <h1 style="text-align: center; margin-top: 25px;">${joke}</h1>
-          <img src="${image}" alt="Chuck Norris" style="display: block; margin: 100px auto 0;"/>
-          <button onclick="reloadJoke()" style="margin-top: 10px;">Get Another Joke</button>
-      </div>
-      <script src="/script.js"></script>
-    `;
+
+    // Log the joke to the terminal
+    console.log(`The joke is: ${joke}`);
+
+    // Read the contents of index.html into a string
+    let html = fs.readFileSync('./public/index.html', 'utf8');
+
+    // Replace `${joke}` with the actual value of `joke`
+    html = html.replace('${joke}', joke);
+
+    // Send the modified HTML to the client
     res.send(html);
-    console.log(joke);
   } catch (error) {
-    // Handle any errors or exceptions that may occur
     console.error(error);
     res.status(500).send('Something went wrong');
   }
