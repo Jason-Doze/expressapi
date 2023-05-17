f918f06f39e8ff99bae7d989ab72fd9b5efc3263
Jason Doze <90075907+Jason-Doze@users.noreply.github.com>
2023-05-17 17:04:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const app = express();
 app.use(cors());
 
 // Set up a middleware to serve static files from the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-
-// Define a new HTTP GET endpoint at /api/joke for the joke data
-app.get('/api/joke', async (req, res) => {
+// Define a new HTTP GET endpoint at /joke for the joke data
+app.get('/joke', async (req, res) => {
   try {
     const response = await fetch('https://api.chucknorris.io/jokes/random');
     const data = await response.json();
