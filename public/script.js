a87d8d30d7fd78ad5fee3cf89cd66d44ffa1f307
Jason Doze <90075907+Jason-Doze@users.noreply.github.com>
2023-05-17 17:05:58 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ function reloadJoke() {
   console.log('Reload button clicked!');
   const jokeElement = document.getElementById('joke');
   jokeElement.innerHTML = 'Loading...';
-  fetch('/api/joke')
+  fetch('/joke')
     .then(response => response.json())
     .then(data => {
       jokeElement.innerHTML = data.joke;
